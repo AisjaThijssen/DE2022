38d57609cceed2c0db7006ea7968dfda081741ab
AisjaThijssen <112868481+AisjaThijssen@users.noreply.github.com>
2022-11-18 13:44:16 +0100

    update files to VM

--- lab8/exercises/kafka-producer/kafkaadmin.py
+++ lab8/exercises/kafka-producer/kafkaadmin.py
@@ -10,8 +10,8 @@ def create_topics(admin, topic_list):
 
 
 if __name__ == '__main__':
-    admin_client = KafkaAdminClient(bootstrap_servers="35.188.85.227:9092",
-                                    client_id='Lab8')  # use your VM's external IP Here!
+    admin_client = KafkaAdminClient(bootstrap_servers="34.133.213.83:9092",
+                                    client_id='Lab8_Ex3')  # use your VM's external IP Here!
     topic_list = [NewTopic(name="game", num_partitions=1, replication_factor=1),
                   NewTopic(name="avg_score", num_partitions=1, replication_factor=1)]
     # delete_topics(admin_client, topics=['avg_score', 'game'])
